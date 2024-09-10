74cabb4005afcd0e87f0a711433d75fe43814a84
Juneroy Quinimon <eroyjune@gmail.com>
2024-09-10 18:51:46 +0800

    feat added mount function to get the blog post using id

--- app/Livewire/BlogEdit.php
+++ app/Livewire/BlogEdit.php
@@ -3,6 +3,7 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\BlogPost;
 
 class BlogEdit extends Component
 {
@@ -14,6 +15,14 @@ class BlogEdit extends Component
         'title' => 'required|string|max:255',
         'content' => 'required|string',
     ];
+
+    public function mount($postId)
+    {
+        $post = BlogPost::findOrFail($postId);
+        $this->postId = $post->id;
+        $this->title = $post->title;
+        $this->content = $post->content;
+    }
 
     public function render()
     {
