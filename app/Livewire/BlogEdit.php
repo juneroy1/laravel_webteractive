76842f02ac89ffd882599cc2303affdbf60ec4b5
Juneroy Quinimon <eroyjune@gmail.com>
2024-09-10 18:43:08 +0800

    feat added rules for title and content

--- app/Livewire/BlogEdit.php
+++ app/Livewire/BlogEdit.php
@@ -9,7 +9,12 @@ class BlogEdit extends Component
     public $postId;
     public $title;
     public $content;
-    
+
+    protected $rules = [
+        'title' => 'required|string|max:255',
+        'content' => 'required|string',
+    ];
+
     public function render()
     {
         return view('livewire.blog-edit');
