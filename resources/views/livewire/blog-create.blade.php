ff21d6002e848da557bd0e340e7d9ffab3549dac
Juneroy Quinimon <eroyjune@gmail.com>
2024-09-10 18:13:50 +0800

    feat: update create blog view page

--- resources/views/livewire/blog-create.blade.php
+++ resources/views/livewire/blog-create.blade.php
@@ -1,3 +1,23 @@
 <div>
-    {{-- Close your eyes. Count to one. That is how long forever feels. --}}
+    @if (session()->has('message'))
+        <div class="alert alert-success">
+            {{ session('message') }}
+        </div>
+    @endif
+
+    <form wire:submit.prevent="createPost">
+        <div class="form-group">
+            <label for="title">Title</label>
+            <input type="text" id="title" wire:model="title" class="form-control">
+            @error('title') <span class="text-danger">{{ $message }}</span> @enderror
+        </div>
+
+        <div class="form-group">
+            <label for="content">Content</label>
+            <textarea id="content" wire:model="content" class="form-control"></textarea>
+            @error('content') <span class="text-danger">{{ $message }}</span> @enderror
+        </div>
+
+        <button type="submit" class="btn btn-primary">Create Post</button>
+    </form>
 </div>
