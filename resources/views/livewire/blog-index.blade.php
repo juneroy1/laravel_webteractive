6073107f2d010d9e49a78f9a76e174bed52b0b89
Juneroy Quinimon <eroyjune@gmail.com>
2024-09-11 00:03:58 +0800

    feat append delete blog component

--- resources/views/livewire/blog-index.blade.php
+++ resources/views/livewire/blog-index.blade.php
@@ -11,6 +11,8 @@
                 <h3>{{ $post->title }}</h3>
                 <p>{{ $post->content }}</p>
                 <a href="{{ route('blog.edit', $post->id) }}" class="btn btn-primary">View Post</a>
+                
+                <livewire:blog-delete :postId="$post->id" :key="$post->id" />
             </div>
         </div>
     @endforeach
