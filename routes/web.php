cc93d0406dd12f7b1b63ee144e7f8e34c164ab99
Juneroy Quinimon <eroyjune@gmail.com>
2024-09-10 19:46:15 +0800

    feat: added routes for BLOG POST CRUD

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Livewire\BlogCreate;
+use App\Livewire\BlogIndex;
+use App\Livewire\BlogEdit;
+use App\Livewire\BlogDelete;
+
+Route::get('/blogs', BlogIndex::class)->name('blog.index');
+Route::get('/blogs/create', BlogCreate::class)->name('blog.create');
+Route::get('/blogs/{id}/edit', BlogEdit::class)->name('blog.edit');
+Route::get('/blogs/{id}/delete', BlogDelete::class)->name('blog.delete');
 Route::get('/', function () {
     return view('welcome');
 });
