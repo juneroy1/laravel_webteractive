4c035f4548d74657778685f7a6c056e561d1815d
Juneroy Quinimon <eroyjune@gmail.com>
2024-09-10 19:45:54 +0800

    feat added content and function for deleting blog

--- resources/views/livewire/blog-delete.blade.php
+++ resources/views/livewire/blog-delete.blade.php
@@ -1,3 +1,11 @@
 <div>
-    {{-- In work, do what you enjoy. --}}
+    <button wire:click="$emit('triggerDelete', {{ $postId }})" class="btn btn-danger">Delete Post</button>
+
+    <script>
+        Livewire.on('triggerDelete', postId => {
+            if(confirm('Are you sure you want to delete this post?')) {
+                @this.call('deletePost');
+            }
+        });
+    </script>
 </div>
