9e1cb6291bffc0cb00988082e1c0cf82c5bf5fba
Juneroy Quinimon <eroyjune@gmail.com>
2024-09-11 00:39:22 +0800

    feat created content and function for vote blade UI

--- resources/views/livewire/vote.blade.php
+++ resources/views/livewire/vote.blade.php
@@ -1,3 +1,11 @@
-<div>
-    {{-- The best athlete wants his opponent at his best. --}}
+<div class="voting-system">
+    <button wire:click="upvote" class="btn btn-success">
+        Upvote ({{ $upvotes }})
+    </button>
+
+    <button wire:click="downvote" class="btn btn-danger">
+        Downvote ({{ $downvotes }})
+    </button>
+
+    <p>Total Votes: {{ $upvotes - $downvotes }}</p>
 </div>
