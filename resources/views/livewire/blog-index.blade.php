69a3d2ecff70e70118fbba39fc637696e6b0e2ae
Juneroy Quinimon <eroyjune@gmail.com>
2024-09-10 18:25:49 +0800

    feat: created a blog index content to display list of post

--- resources/views/livewire/blog-index.blade.php
+++ resources/views/livewire/blog-index.blade.php
@@ -1,3 +1,13 @@
 <div>
-    {{-- Be like water. --}}
+    <h1>All Blog Posts</h1>
+
+    @foreach($posts as $post)
+        <div class="card mt-3">
+            <div class="card-body">
+                <h3>{{ $post->title }}</h3>
+                <p>{{ $post->content }}</p>
+                <a href="{{ route('blog.show', $post) }}" class="btn btn-primary">View Post</a>
+            </div>
+        </div>
+    @endforeach
 </div>
