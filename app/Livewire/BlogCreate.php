6d77687eb6a93ea40b5457c3ae3b55c63e33ef18
Juneroy Quinimon <eroyjune@gmail.com>
2024-09-10 17:47:16 +0800

    feat created 2 fields for creating blog in BlogCreate class

--- app/Livewire/BlogCreate.php
+++ app/Livewire/BlogCreate.php
@@ -6,6 +6,9 @@ use Livewire\Component;
 
 class BlogCreate extends Component
 {
+    public $title;
+    public $content;
+    
     public function render()
     {
         return view('livewire.blog-create');
