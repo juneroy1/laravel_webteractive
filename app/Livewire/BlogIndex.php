bb1633b71b7153a98e77b782840a79e19de89e20
Juneroy Quinimon <eroyjune@gmail.com>
2024-09-11 02:38:32 +0800

    feat added filter for admin show all posts else show only post created by user

--- app/Livewire/BlogIndex.php
+++ app/Livewire/BlogIndex.php
@@ -8,10 +8,19 @@ class BlogIndex extends Component
 {
     // this was working before a listeners
     // protected $listeners = ['postDeleted' => 'render'];
+   
     public function render()
     {
+        $posts = [];
+         // Admin can access all posts, users can only access their own
+        if (auth()->user()->isAdmin()) {
+            $posts = BlogPost::latest()->paginate(5);
+        } else {
+            $posts = BlogPost::where('user_id', auth()->id())->paginate(5);
+        }
+
         return view('livewire.blog-index', [
-            'posts' => BlogPost::latest()->paginate(1),
+            'posts' => $posts,
         ]);
     }
 }
