4afd0742be51a05badcf35bc9dbb879e023ce48b
Juneroy Quinimon <eroyjune@gmail.com>
2024-09-11 01:28:02 +0800

    feat create new role column for user table default User

--- database/migrations/2024_09_10_172559_add_role_to_users_table.php
+++ database/migrations/2024_09_10_172559_add_role_to_users_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             //
+            $table->string('role')->default('User');
         });
     }
 
@@ -23,6 +24,7 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             //
+            $table->dropColumn('role');
         });
     }
 };
