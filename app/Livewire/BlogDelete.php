a0753faf0b737fbf969734271b72e113e46a4e4c
Juneroy Quinimon <eroyjune@gmail.com>
2024-09-10 19:03:57 +0800

    feat created postid and added deletePost function for deleting blog post

--- app/Livewire/BlogDelete.php
+++ app/Livewire/BlogDelete.php
@@ -6,6 +6,17 @@ use Livewire\Component;
 
 class BlogDelete extends Component
 {
+    public $postId;
+
+    public function deletePost()
+    {
+        $post = BlogPost::findOrFail($this->postId);
+        $post->delete();
+
+        session()->flash('message', 'Blog post deleted successfully.');
+        return redirect()->route('blog.index');
+    }
+
     public function render()
     {
         return view('livewire.blog-delete');
