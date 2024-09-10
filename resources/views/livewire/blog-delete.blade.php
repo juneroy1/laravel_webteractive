fc97bea991ad74c9bfd7b7467c65acd995da28df
Juneroy Quinimon <eroyjune@gmail.com>
2024-09-10 20:58:14 +0800

    uncomment live wire in script for now and use form submit for testing

--- resources/views/livewire/blog-delete.blade.php
+++ resources/views/livewire/blog-delete.blade.php
@@ -1,11 +1,13 @@
 <div>
-    <button wire:click="$emit('triggerDelete', {{ $postId }})" class="btn btn-danger">Delete Post</button>
-
+    <!-- <button wire:click="deletePost" class="btn btn-danger">Delete Post</button> -->
+    <form wire:submit.prevent="deletePost">
+        <button type="submit" class="btn btn-primary">Delete Post</button>
+    </form>
     <script>
-        Livewire.on('triggerDelete', postId => {
-            if(confirm('Are you sure you want to delete this post?')) {
-                @this.call('deletePost');
-            }
-        });
-    </script>
+        // Livewire.on('triggerDelete', postId => {
+        //     if (confirm('Are you sure you want to delete this post?')) {
+        //         @this.call('deletePost');
+        //     }
+        // });
+</script>
 </div>
