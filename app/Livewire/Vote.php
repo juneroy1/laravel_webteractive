db949cdd3cebaf84ccbbe5fd31653633c06109ae
Juneroy Quinimon <eroyjune@gmail.com>
2024-09-11 00:36:20 +0800

    feat added variable, mount update and downvote function

--- app/Livewire/Vote.php
+++ app/Livewire/Vote.php
@@ -6,6 +6,29 @@ use Livewire\Component;
 
 class Vote extends Component
 {
+    public $post;
+    public $upvotes;
+    public $downvotes;
+
+    public function mount(BlogPost $post)
+    {
+        $this->post = $post;
+        $this->upvotes = $post->upvotes;
+        $this->downvotes = $post->downvotes;
+    }
+
+    public function upvote()
+    {
+        $this->post->increment('upvotes');
+        $this->upvotes = $this->post->upvotes;
+    }
+
+    public function downvote()
+    {
+        $this->post->increment('downvotes');
+        $this->downvotes = $this->post->downvotes;
+    }
+
     public function render()
     {
         return view('livewire.vote');
