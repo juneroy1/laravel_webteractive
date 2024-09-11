24806078e6ae63a82785e2322653736eadb6e62b
Juneroy Quinimon <eroyjune@gmail.com>
2024-09-11 19:23:02 +0800

    revert: changes in web.php

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,16 @@ Route::get('/blogs', BlogIndex::class)->name('blog.index');
 Route::get('/blogs/create', BlogCreate::class)->name('blog.create');
 Route::get('/blogs/{id}/edit', BlogEdit::class)->name('blog.edit');
 Route::get('/blogs/{postId}/delete', BlogDelete::class)->name('blog.delete');
-Route::get('/',BlogIndex::class)->name('blog.index');
+Route::get('/', function () {
+    return view('welcome');
+});
 
-
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
