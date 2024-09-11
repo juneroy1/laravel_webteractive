4c26eea83122547b3007cd5e426f182c5955b6a1
Juneroy Quinimon <eroyjune@gmail.com>
2024-09-11 21:04:00 +0800

    feat created new test class for blog delete

---
+++ tests/Unit/BlogDeleteTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Unit;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use Livewire\Livewire;
+use App\Models\BlogPost;
+use App\Livewire\BlogDelete;
+use App\Models\User;
+class BlogDeleteTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function it_can_delete_a_blog_post()
+    {
+        // Create a user
+        $user = User::factory()->create(['role' => 'User']);
+
+        // Create a blog post associated with the user
+        $post = BlogPost::factory()->create([
+            'title' => 'Original Title',
+            'content' => 'Original content.',
+        ]);
+
+        // Simulate the user being logged in
+        $this->actingAs($user);
+
+        // Test the Livewire component for updating the blog post
+        Livewire::test(BlogDelete::class, ['postId' => $post->id])
+            ->call('deletePost');
+
+      
+    }
+}
