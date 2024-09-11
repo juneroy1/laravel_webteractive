8df7dbe207c11e86775c45cc76ca00507bd96944
Juneroy Quinimon <eroyjune@gmail.com>
2024-09-12 04:20:26 +0800

    update icon for update and downvote

--- resources/views/livewire/vote.blade.php
+++ resources/views/livewire/vote.blade.php
@@ -8,7 +8,9 @@
     <div class="flex items-center mr-6">
          <button wire:click="upvote" class="btn btn-success">
             <div class="flex">
-                <svg class="w-5 h-5 mr-1" fill="currentColor" viewBox="0 0 24 24"><path d="M14 9V5a3 3 0 00-3-3 3 3 0 00-3 3v4a3 3 0 00-3 3v7a3 3 0 003 3h6a3 3 0 003-3v-7a3 3 0 00-3-3zM9 5a2 2 0 114 0v4H9V5zm7 16h-6a2 2 0 01-2-2v-7a2 2 0 012-2h6a2 2 0 012 2v7a2 2 0 01-2 2z"></path></svg>
+                <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 15l7-7 7 7"></path>
+                </svg>
                 <span>{{ $post->upvotes }}</span>
             </div>
         </button>
@@ -16,7 +18,9 @@
     <div class="flex items-center">
          <button wire:click="downvote" class="btn btn-success">
             <div class="flex">
-                <svg class="w-5 h-5 mr-1" fill="currentColor" viewBox="0 0 24 24"><path d="M12 21c-4.97 0-9-4.03-9-9s4.03-9 9-9 9 4.03 9 9-4.03 9-9 9zm0-16c-3.86 0-7 3.14-7 7s3.14 7 7 7 7-3.14 7-7-3.14-7-7-7z"></path><path d="M15.99 12l-3.42-3.42L10 11.59 6.41 8 5 9.41l5 5 7-7z"></path></svg>
+                 <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
+                </svg>
                 <span>{{ $post->downvotes }}</span>
             </div>
         </button>
