41c3e3f78b42fbd0bf119b4a5202d6f3676a8b8e
Juneroy Quinimon <eroyjune@gmail.com>
2024-09-12 04:08:00 +0800

    feat: update routes

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,7 @@ use App\Livewire\BlogShow;
 
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Route::middleware([
     'auth:sanctum',
@@ -22,7 +20,9 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
-
+    Route::get('/', function () {
+        return view('dashboard');
+    })->name('blog.index');
     Route::get('/blogs', BlogIndex::class)->name('blog.index');
     Route::get('/blogs/create', BlogCreate::class)->name('blog.create');
     Route::get('/blogs/{id}/edit', BlogEdit::class)->name('blog.edit');
