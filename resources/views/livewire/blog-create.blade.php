3882320fb9122a08762566e4698be3cf0c3b99b7
Juneroy Quinimon <eroyjune@gmail.com>
2024-09-12 03:46:08 +0800

    hotfix change button color to black

--- resources/views/livewire/blog-create.blade.php
+++ resources/views/livewire/blog-create.blade.php
@@ -20,6 +20,6 @@
             @error('content') <span class="text-red-500">{{ $message }}</span> @enderror
         </div>
 
-        <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Create Post</button>
+        <button type="submit" class="bg-blue-500 text-black px-4 py-2 rounded hover:bg-blue-600">Create Post</button>
     </form>
 </div>
