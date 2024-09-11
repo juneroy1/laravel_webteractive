5a286b560374525095d546414279cd966a45e4eb
Juneroy Quinimon <eroyjune@gmail.com>
2024-09-11 21:51:00 +0800

    feat: dont allow the unauthenticated user to access the blog api or routes

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,8 @@ use App\Livewire\BlogIndex;
 use App\Livewire\BlogEdit;
 use App\Livewire\BlogDelete;
 
-Route::get('/blogs', BlogIndex::class)->name('blog.index');
-Route::get('/blogs/create', BlogCreate::class)->name('blog.create');
-Route::get('/blogs/{id}/edit', BlogEdit::class)->name('blog.edit');
-Route::get('/blogs/{postId}/delete', BlogDelete::class)->name('blog.delete');
+
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -23,4 +21,9 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+
+    Route::get('/blogs', BlogIndex::class)->name('blog.index');
+    Route::get('/blogs/create', BlogCreate::class)->name('blog.create');
+    Route::get('/blogs/{id}/edit', BlogEdit::class)->name('blog.edit');
+    Route::get('/blogs/{postId}/delete', BlogDelete::class)->name('blog.delete');
 });
