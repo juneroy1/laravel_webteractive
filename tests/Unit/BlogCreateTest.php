0862f50ab917c5c705a28505010033977d558c72
Juneroy Quinimon <eroyjune@gmail.com>
2024-09-11 19:01:26 +0800

    feat added function test for user can only create their own blog

--- tests/Unit/BlogCreateTest.php
+++ tests/Unit/BlogCreateTest.php
@@ -27,5 +27,20 @@ class BlogCreateTest extends TestCase
 
         $this->assertDatabaseHas('blog_posts', ['title' => 'Test Blog Post']);
     }
+     /** @test */
+    public function a_user_can_only_create_their_own_blog_post()
+    {
+        $user = User::factory()->create(['role' => 'User']);
+
+        $this->actingAs($user);
+
+        Livewire::test(BlogCreate::class)
+            ->set('title', 'User Blog Post')
+            ->set('content', 'This is a User blog post.')
+            ->call('createPost')
+            ->assertRedirect(route('blog.index'));
+
+        $this->assertDatabaseHas('blog_posts', ['title' => 'User Blog Post']);
+    }
 }
 
