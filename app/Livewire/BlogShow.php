487ca73f8a1478faec833ecddf9c3be05be3cf74
Juneroy Quinimon <eroyjune@gmail.com>
2024-09-12 03:25:02 +0800

    feat: added variable function mount in the Blogshow class

--- app/Livewire/BlogShow.php
+++ app/Livewire/BlogShow.php
@@ -3,9 +3,16 @@
 namespace App\Livewire;
 
 use Livewire\Component;
-
+use App\Models\BlogPost;
 class BlogShow extends Component
 {
+    public $post;
+
+    public function mount($id)
+    {
+        $this->post = BlogPost::findOrFail($id);
+    }
+
     public function render()
     {
         return view('livewire.blog-show');
