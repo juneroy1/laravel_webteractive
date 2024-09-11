2314115b412b1c75f531b933073ea04e0f3dfe52
Juneroy Quinimon <eroyjune@gmail.com>
2024-09-11 17:37:25 +0800

    feat created blogpost factory

---
+++ database/factories/BlogPostFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\BlogPost;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class BlogPostFactory extends Factory
+{
+    protected $model = BlogPost::class;
+
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence,
+            'content' => $this->faker->paragraph,
+            'user_id' => \App\Models\User::factory(), // Assuming each post belongs to a user
+        ];
+    }
+}
+
