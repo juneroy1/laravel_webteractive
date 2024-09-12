b88fb3c81864891b3e946db6200a569d095cd14e
Juneroy Quinimon <eroyjune@gmail.com>
2024-09-12 11:28:05 +0800

    feat created function for getting image url attribute

--- app/Models/BlogPost.php
+++ app/Models/BlogPost.php
@@ -5,15 +5,20 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\User;
-
+use Illuminate\Support\Facades\Storage;
 class BlogPost extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'content', 'user_id'];
+    protected $fillable = ['title', 'content', 'user_id','image'];
 
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function getImageUrlAttribute()
+    {
+        return $this->image ? Storage::url($this->image) : null;
     }
 }
